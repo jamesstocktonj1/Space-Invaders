6fa431e276565a7b3a7a707e6e8d584497e388a2
js3g20 <js3g20@soton.ac.uk>
2022-04-18 16:50:15 +0100

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -2,7 +2,7 @@
 
 
 
-uint16_t invader[INVADER_W][INVADER_H] = {
+uint16_t invaderA[INVADER_W][INVADER_H] = {
     O, O, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, O, O, 
     O, O, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, O, O, 
     O, O, O, O, O, O, P, P, O, O, O, O, O, O, P, P, O, O, O, O, O, O, 
@@ -21,31 +21,31 @@ uint16_t invader[INVADER_W][INVADER_H] = {
     O, O, O, O, O, O, P, P, P, P, O, O, P, P, P, P, O, O, O, O, O, O
 };
 
-uint16_t invadera[INVADER_W][INVADER_H] = {
-    RED, 
+uint16_t invaderB[INVADER_W][INVADER_H] = {
+    O, O, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, O, O, 
+    P, P, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, P, P, 
+    P, P, O, O, O, O, P, P, O, O, O, O, O, O, P, P, O, O, O, O, P, P, 
+    P, P, O, O, O, O, P, P, O, O, O, O, O, O, P, P, O, O, O, O, P, P, 
+    P, P, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, P, P, 
+    P, P, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, P, P, 
+    O, O, P, P, P, P, O, O, P, P, P, P, P, P, O, O, P, P, P, P, O, O, 
+    O, O, P, P, P, P, O, O, P, P, P, P, P, P, O, O, P, P, P, P, O, O, 
+    O, O, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, O, O, 
+    O, O, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, O, O, 
+    O, O, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, O, O, 
+    O, O, O, O, P, P, P, P, P, P, P, P, P, P, P, P, P, P, O, O, O, O, 
+    O, O, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, O, O, 
+    O, O, O, O, P, P, O, O, O, O, O, O, O, O, O, O, P, P, O, O, O, O,  
+    O, O, P, P, O, O, O, O, O, O, O, O, O, O, O, O, O, O, P, P, O, O, 
+    O, O, P, P, O, O, O, O, O, O, O, O, O, O, O, O, O, O, P, P, O, O
 };
 
-/*
-uint16_t invader[INVADER_H][INVADER_W] = {
-
-};
-*/
-/*
-uint16_t invader[INVADER_W][INVADER_H] = {
-    O, O, P, O, O, O, O, O, P, O, O,
-    O, O, O, P, O, O, O, P, O, O, O,
-    O, O, P, P, P, P, P, P, P, O, O,
-    O, P, P, O, P, P, P, O, P, P, O,
-    P, P, P, P, P, P, P, P, P, P, P,
-    P, O, P, P, P, P, P, P, P, O, P,
-    P, O, P, O, O, O, O, O, P, O, P,
-    O, O, O, P, P, O, P, P, O, O, O
-};*/
 
 
-void draw_invader(uint8_t i, uint8_t j) {
+void draw_invader(uint8_t i, uint8_t j, uint8_t type) {
 
     uint16_t x, y;
+
 	write_cmd(COLUMN_ADDRESS_SET);
 	write_data16(i);
 	write_data16(i + INVADER_W - 1);
@@ -53,7 +53,15 @@ void draw_invader(uint8_t i, uint8_t j) {
 	write_data16(j);
 	write_data16(j + INVADER_H - 1);
 	write_cmd(MEMORY_WRITE);
-    for(x=0; x<INVADER_W; x++)
-        for(y=0; y<INVADER_H; y++)
-			write_data16(invader[x][y]);
+
+    for(x=0; x<INVADER_W; x++) {
+        for(y=0; y<INVADER_H; y++) {
+            if(type) {
+                write_data16(invaderB[x][y]);
+            }
+            else {
+                write_data16(invaderA[x][y]);
+            }
+        }
+    }
 }
