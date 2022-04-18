b21a536390539561e0c707d2a79bc5f87c0b1511
js3g20 <js3g20@soton.ac.uk>
2022-04-18 22:49:25 +0100

    Create game.h

---
+++ include/game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "graphics.h"
+
+
+
+#define ROW_INCREMENT 15
+#define COLUMN_INCREMENT 6
+
+
+#define ROW_COUNT 6
+
+#define ITEM_WIDTH 30
+
+
+#define PADDING 5
+#define ROW_HEIGHT 20
+
+#define ROW_WIDTH(count) (count * (PADDING + INVADER_W)) + PADDING 
+
+
+#endif
