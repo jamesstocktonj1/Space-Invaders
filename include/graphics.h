1ba77d241fca5fb080c3992b0884f74c41d2c589
js3g20 <js3g20@soton.ac.uk>
2022-04-18 22:49:00 +0100

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -3,6 +3,7 @@
 
 #include "../lib/lcd.h"
 #include "../lib/ili934x.h"
+#include "game.h"
 
 
 #define INVADER_W 22
@@ -11,16 +12,15 @@
 #define INVADER_COL WHITE
 
 
-
+//simplified for graphics array
 #define P INVADER_COL
 #define O 0x0000
 
-uint16_t invaderA[INVADER_W][INVADER_H];
-uint16_t invaderB[INVADER_W][INVADER_H];
 
-
+extern uint16_t invaderA[INVADER_W][INVADER_H];
+extern uint16_t invaderB[INVADER_W][INVADER_H];
 
 void draw_invader(uint8_t i, uint8_t j, uint8_t type);
-
+void draw_screen(uint8_t x, uint8_t y, uint8_t type, uint8_t* array, uint8_t arrayLen);
 
 #endif
