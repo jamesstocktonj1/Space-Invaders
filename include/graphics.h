874fd97d97bad4789790d2079d741905bab0cf17
js3g20 <js3g20@soton.ac.uk>
2022-04-19 21:29:50 +0100

    Update graphics.h

--- include/graphics.h
+++ include/graphics.h
@@ -9,6 +9,9 @@
 #define INVADER_W 22
 #define INVADER_H 16
 
+#define BLASTER_W 22
+#define BLASTER_H 14
+
 #define INVADER_COL WHITE
 
 
@@ -20,7 +23,12 @@
 extern uint16_t invaderA[INVADER_W][INVADER_H];
 extern uint16_t invaderB[INVADER_W][INVADER_H];
 
+extern uint16_t blaster[BLASTER_W][BLASTER_H];
+
 void draw_invader(uint8_t i, uint8_t j, uint8_t type);
 void draw_screen(uint8_t x, uint8_t y, uint8_t type, uint8_t* array, uint8_t arrayLen);
+
+void draw_blaster(uint8_t i);
+rectangle get_blaster_outline(uint8_t i);
 
 #endif
